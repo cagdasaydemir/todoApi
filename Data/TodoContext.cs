379e8f549a0ac23f9c14978dbbfd6568ab085c3f
cagdasaydemir <ac.cagdasaydemir@gmail.com>
2022-05-19 23:58:18 +0300

    Initial commit

---
+++ Data/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todoApi.Entities;
+
+namespace todoApi.Data
+{
+    public class TodoContext : DbContext
+
+    {
+            public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+            public DbSet<Todo> Todos { get; set; }
+        }
+    }
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# todoApi
+ todo api for reactTodoApp
